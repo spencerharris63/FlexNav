ee8eadf2d66c08f9cdfbd476dc0d1405b03f4d3f
spencerharris63 <harris.spencer63@gmail.com>
2023-06-06 18:25:40 -0400

    message to self

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,42 @@
-// scripts go here
+var tabs = document.querySelectorAll("nav a");
+var contentPara = document.querySelector(".content");
+
+function makeActive(type) {
+  makeInactive();
+  var tabToActivate = document.querySelector(`a[href="#${type}"]`)
+  console.log('WHEW', tabToActivate);
+  tabToActivate.classList.add('active');
+}
+
+function makeInactive() {
+  tabs.forEach((tab) => tab.classList.remove("active"));
+}
+
+function setContentAccordingToHash() {
+  const type = window.location.hash.substring(1);
+  for (var item of data) {
+    if (item.section === type) {
+      // contentPara.innerHTML = item.story
+      contentPara.innerHTML = `<h2>${item.section}</h2> <p>${item.story}</p>`;
+      setActiveTabAccordingToHash(type);
+    }
+  }
+}
+
+function initializePage() {
+    // set some defaults if there is no hash
+    if (!window.location.hash) {
+      window.location.hash = "cuisines";
+      document.querySelector('[href="#cuisines"]').classList.add("active");
+    } else {
+      // if there is a hash set the active tab accordingly
+      document
+        .querySelector(`[href="${window.location.hash}"]`)
+        .classList.add("active");
+    }
+    setContentAccordingToHash();
+}
+
+// document.addEventListener("click", makeActive);
+document.addEventListener("DOMContentLoaded", initializePage);
+
